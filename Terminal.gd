bed07b5dc48e187397abe4ea3a8c1b58b3e84a82
San <istamarahsan@gmail.com>
2023-02-16 04:16:37 +0700

    remove morale

--- Terminal.gd
+++ Terminal.gd
@@ -6,9 +6,6 @@ signal event_option_selected()
 func set_hull(proportion: float) -> void:
 	pass
 	
-func set_morale(proportion: float) -> void:
-	pass
-	
 func set_energy(proportion: float) -> void:
 	pass
 
